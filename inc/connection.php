a36914170ab5a3ee1e13d897c5a5380db304483e
Martin Kennedy <kennedym103@gmail.com>
2018-01-03 17:03:28 -0500

    initial files created

---
+++ inc/connection.php
@@ -0,0 +1,11 @@
+<?php
+//new PHP Data Object //This allows PHP to interact with a database
+try{
+  $dsn = "mysql:host=localhost;dbname=timetracker;port=8889"; $username = "root"; $password = "root";
+  //(For local use, PHPMyAdmin doesn't use a password.)
+  $db = new PDO($dsn, $username, $password);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(Exception $e) {
+  echo $e->getMessage();
+  exit;
+}
