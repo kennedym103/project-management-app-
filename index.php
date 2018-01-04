fc27083871bbf68be16bc3622b3b16ea46ded35c
kennedym103 <kennedym103@gmail.com>
2018-01-04 15:01:47 -0500

    beginning of styling work

--- index.php
+++ index.php
@@ -7,38 +7,44 @@ $page = null;
 include 'inc/header.php';
 ?>
 	<div class="section catalog random">
-    <div class="col-container actions-container">
-
-      <h1>Welcome</h1>
-      <p class="actions-copy">What would you like to do today?</p>
+    <div class="col-container actions-container asdf">
       <div class="actions-wrapper">
-        <ul class="actions">
-          <li class="actions-item">
+        <div class="actions">
+          <div id="cl" class="actions-item action-left">
             <a class="actions-link" href="task.php">
-              <span class="actions-icon">
+              <span  class="actions-icon">
                 <svg viewbox="0 0 64 64"><use xlink:href="#task_icon"></use></svg>
               </span>
               Add Task
             </a>
-          </li>
-          <li class="actions-item">
+          </div>
+          <div class="actions-item action-center">
             <a class="actions-link" href="project.php">
               <span class="actions-icon">
                 <svg viewbox="0 0 64 64"><use xlink:href="#project_icon"></use></svg>
               </span>
               Add Project
             </a>
-          </li>
-          <li class="actions-item">
+          </div>
+          <div id="cr" class="actions-item action-right">
             <a class="actions-link" href="reports.php">
               <span class="actions-icon">
                 <svg viewbox="0 0 64 64"><use xlink:href="#report_icon"></use></svg>
               </span>
               View Reports
             </a>
-          </li>
-        </ul>
+          </div>
+					<div class="circle">
+						<div class="inner-circle">
+							<div class="triangle"></div>
+						</div>
+					</div>
+        </div>
       </div>
+			<div class="text-container">
+				<h1>Welcome</h1>
+				<p class="actions-copy">What would you like to do today?</p>
+			</div>
 
 		</div>
 
